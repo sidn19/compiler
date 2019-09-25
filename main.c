ca1de425b787629dec6dcd2553797bd9bad04f28
Sidharth Nair <54242817+sidn19@users.noreply.github.com>
2019-09-25 11:46:05 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "stack.h"
+
+int validateSemicolons(FILE*, Node*, int*);
+
+int main() {
+	FILE *file;
+	int line = 1;
+	Node top;
+	file = fopen("./test.c", "r");
+	if (validateSemicolons(file, &top, &line)) {
+		printf("No semicolon errors detected");
+	}
+	else {
+		printf("Semicolon error detected at line %d", line);
+	}
+	fclose(file);
+	return 0;
+}
+
+int validateSemicolons(FILE *file, Node* top, int* line) {
+	char ch;
+	top = NULL;
+	while((ch = getc(file)) != EOF) {
+		if (ch != ';') {
+			if (ch == '\n') *line = *line + 1;
+			else if (ch == '#') {
+				while(getc(file) != '\n');
+				*line = *line + 1;
+			}
+			else if (ch == '{') {
+				emptyStack(&top);
+			}
+			else push(&top, ch);
+		}
+		else {
+			if ((peek(top) < 'A' || peek(top) > 'Z') ||
+				(peek(top) < 'a' || peek(top) > 'z')/* || peek(top) == ')'*/) {
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
