1a00e2d0ea452b46c49835c3e18ae04cc5814f96
Aditya Nair <33800818+ArcTLK@users.noreply.github.com>
2019-10-08 18:24:49 +0530

    Update main.c

--- main.c
+++ main.c
@@ -23,32 +23,60 @@ int validateSemicolons(FILE *file, Node* top, int* line) {
 	top = NULL;
 	while((ch = getc(file)) != EOF) {
 		if (ch != ';') {
-			if (ch == '\n') {
-				if (peek(top) != ';') {
-					while (!isEmpty(top) && peek(top) == ' ') {
-						pop(&top);
-					}
-					if (!isEmpty(top)) return 0;
+			if (ch == '\n') (*line)++; //increase line number
+			/* semicolon should be present before function invocation */
+			else if (ch == '(') {
+				/* ignore function name (all characters except whitespace) */
+				while(!isEmpty(top) && (peek(top) != ' ' && peek(top) != '\t' && peek(top) != '\n')) pop(&top);
+				/* now ignore whitespace */
+				while(!isEmpty(top) && (peek(top) == ' ' || peek(top) == '\t' || peek(top) == '\n')) pop(&top);
+				if (!isEmpty(top) && peek(top) != ';' && peek(top) != '{' && peek(top) != '}' &&
+				/* ignoring int of int main */
+				 peek(top) != 't' && peekFromTop(top, 1) != 'n' && peekFromTop(top, 2) != 'i') {
+					return 0;
 				}
-				*line = *line + 1;
 			}
+			/* function invocations must end with semicolon */
+			else if (ch == ')') {
+				//ignore whitespace
+				while((ch = getc(file)) == ' ' || ch == '\n' || ch == '\t') {
+					if (ch == '\n') (*line)++;
+				}
+				if (ch != ';' && ch != '{') return 0;
+				else push(&top, ch);
+			}
+			/* ignore all preprocessor statments */
 			else if (ch == '#') {
 				while(getc(file) != '\n');
-				*line = *line + 1;
+				(*line)++;
 			}
-			else if (ch == '{') {
-				emptyStack(&top);
-			}
+			/* ignore for loop */
 			else if (ch == 'r' && peek(top) == 'o' && peekFromTop(top, 1) == 'f') {
 				while(getc(file) != '\n');
-				*line = *line + 1;
+				(*line)++;
 			}
+			/* ignore if statement */
+			else if (ch == 'f' && peek(top) == 'i') {
+				while(getc(file) != '\n');
+				(*line)++;
+			}
+			/* push to stack */
 			else push(&top, ch);
 		}
 		else {
+			/* semicolons cant be anywhere other than identifiers and function invocations */
+			/* eg: a = a + ; is invalid */
+			/* ignore whitespace */
+			while(!isEmpty(top) && (peek(top) == ' ' || peek(top) == '\t' || peek(top) == '\n')) pop(&top);
+			
 			if (((peek(top) < 'A' || peek(top) > 'Z') &&
 				(peek(top) < 'a' || peek(top) > 'z') && (peek(top) < '0' || peek(top) > '9')) && peek(top) != ')') {
 				return 0;
+			}
+			else {
+				//no errors, empty stack
+				emptyStack(&top);
+				push(&top, ';');
 			}
 		}
 	}
