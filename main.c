97c20c62ee4d9c6add0ae48fecafa16e5ec06b9d
Sidharth Nair <54242817+sidn19@users.noreply.github.com>
2019-10-11 12:44:15 +0530

    Update main.c

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main() {
 		printf("No semicolon errors detected");
 	}
 	else {
-		printf("Semicolon error detected at line %d", line-1);
+		printf("Semicolon error detected at line %d", line);
 	}
 	fclose(file);
 	return 0;
